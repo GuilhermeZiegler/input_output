7e77d17c471c296ee02cf254e4b9d188b650b78d
GuiTrintinalia <guilhermetrintinalia@gmail.com>
2024-07-26 17:50:11 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+[build-system]
+requires = ["setuptools", "wheel"]
+build-backend = "setuptools.build_meta"
+
+[project]
+name = "meu_pacote"
+version = "0.1.0"
+description = "Uma breve descrição do seu pacote"
+authors = [{ name = "Seu Nome", email = "seu.email@exemplo.com" }]
+readme = "README.md"
+license = { text = "MIT" }
+python-required = ">=3.6"
+
+[project.urls]
+"Homepage" = "https://github.com/seu-usuario/meu_pacote"
